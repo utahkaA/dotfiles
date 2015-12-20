3b1b2f003575575c121f0fda454bdd68f0a03dc5
utahkaA <utahka.akiba@gmail.com>
2015-12-21 01:22:40 +0900

    added the .tar-exclude
    - added "grep --color"
    - update README.md
    - update dotfileln.sh

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 dotfiles
 ========
-
+Backup with `tar` command
+-------------------------
+```
+tar zcvf hoge.tar.gz --exclude-from=${HOME}/.tar-exclude hoge/
+```
--- dotfilesln.sh
+++ dotfilesln.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.emacs.el ~/.emacs.el
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.latexmkrc ~/.latexmkrc
+ln -sf ~/dotfiles/.tar-exclude ~/.tar-exclude
